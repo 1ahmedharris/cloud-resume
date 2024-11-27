73d03dab08de53dba10957f9ff5dda62106e3631
Ahmed Harris <24aitc@gmail.com>
2024-11-27 03:14:00 -0500

    Update function.py

--- function.py
+++ function.py
@@ -10,15 +10,12 @@ def lambda_handler(event, context):
     views = int(response['Item']['views']) 
     views += 1
     
-    
-    
     response = table.update_item(
         Key={'id':'1'},
         UpdateExpression='SET #v = :val',
         ExpressionAttributeNames={'#v': 'views'},
         ExpressionAttributeValues={':val': views}
     )
-
 
     return {
     'statusCode': 200,
